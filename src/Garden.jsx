e396a661256a47a1d7f8c5a824a1508fb44cba1c
Lisa <lisomrr@gmail.com>
2018-12-16 02:12:21 +0300

    notso broken timer

--- src/Garden.jsx
+++ src/Garden.jsx
@@ -6,11 +6,10 @@ import {
     setNewCartState,
     setNewFlowersStateInTheShop,
 } from "./actions/game-actions";
-import moment from 'moment';
 import Paper from "@material-ui/core/es/Paper/Paper";
 import Chip from "@material-ui/core/es/Chip/Chip";
 import Avatar from "@material-ui/core/es/Avatar/Avatar";
-import {Flower} from "./Flower";
+import {Aa} from "./Flower";
 import Button from "@material-ui/core/es/Button/Button";
 
 
@@ -28,16 +27,6 @@ class Garden extends React.Component {
         let recountCart = this.props.cartState !== 0 ? this.props.cartState - 1 : this.props.cartState;
         this.props.setNewCartState(recountCart);
         this.props.plantFlower();
-    };
-
-    az = (value) => {
-        return value >=0  ? this.setState({counter: value}): 0
-
-    };
-
-    timer = (value = 60, delay = 4000) => {
-        let timer = setInterval(this.az(--value), delay);
-        this.setState({timer});
     };
 
 
@@ -66,8 +55,8 @@ class Garden extends React.Component {
                 <Paper style={{width: 400, height: 400, margin: 20, padding: 20, background: 'rgb(97, 132, 60)'}}
                        elevation={1}>
                     <div>{this.props.growingFlowers.map((item, idx) => {
-                        return <div><Flower key={idx}/>
-                            <div>{this.timer()}</div>
+                        return <div><Aa key={idx}/>
+
                         </div>
                     })}</div>
                 </Paper>
