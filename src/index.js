7d6c356a57b23b06e0780a5aa3c9f65cea8cd0bb
Lisa <lisomrr@gmail.com>
2018-12-17 23:23:18 +0300

    added redux-persist for saving data

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 import './index.css';
 import App from './components/App';
 import reducer from './reducers/game-reducer';
 import * as serviceWorker from './serviceWorker';
+import {PersistGate} from 'redux-persist/integration/react'
+import {persistStore, persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 
-const store = createStore(
-    reducer,
+const persistConfig = {
+    key: 'root',
+    storage,
+};
 
-);
+const persistedReducer = persistReducer(persistConfig, reducer);
 
+let store = createStore(persistedReducer);
+let persistor = persistStore(store);
 
+console.log(store);
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App/>
+        </PersistGate>
     </Provider>,
     document.getElementById('root'),
 );
